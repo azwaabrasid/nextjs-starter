9bdd0779f9635491e5fab50bbfa9ed84dcf35cb0
Azwa Abrasid <azwa.abrasid@gmail.com>
2022-06-28 16:35:29 +0800

    chore: set tsconfig path resolution

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 
-import '../../styles/globals.scss';
+import 'styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <div className="app">
